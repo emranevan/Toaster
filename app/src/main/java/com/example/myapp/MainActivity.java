6a19de91da5acc8fb7f526e07796e08a8879c117
emranevan <77226225+emranevan@users.noreply.github.com>
2021-01-17 16:38:04 +0600

    new build

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
-
-import com.example.toastlibrary.Toaster;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,8 +22,15 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, )
-                Toaster.toaster(MainActivity.this,"This is a toast");
+                /*Intent intent = new Intent(MainActivity.this, NewActivity.class);
+                Toaster.toaster(MainActivity.this,"This is a toast");*/
+                Toast toast = new Toast(MainActivity.this);
+                toast.setDuration(Toast.LENGTH_LONG);
+
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View view = inflater.inflate(R.layout.toast_layout, null);
+                toast.setView(view);
+                toast.show();
             }
         });
     }
--- toastlibrary/src/main/java/com/example/toastlibrary/Toaster.java
+++ toastlibrary/src/main/java/com/example/toastlibrary/Toaster.java
@@ -1,17 +1,44 @@
 package com.example.toastlibrary;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class Toaster {
-    public static void toaster(Context context, String message){
+    public static void successToast(Context context, String message){
 
         Toast toast = Toast.makeText(context, message, Toast.LENGTH_LONG);
         View view = toast.getView();
-        view.setBackgroundResource(R.drawable.toast_bg);
+        int bg_color = Color.GREEN;
+        view.getBackground().setColorFilter(bg_color, PorterDuff.Mode.SRC_IN);
+        TextView text = view.findViewById(android.R.id.message);
+        text.setTextColor(Color.WHITE);
+        toast.show();
+    }
+
+    public static void errorToast(Context context, String message){
+
+        Toast toast = Toast.makeText(context, message, Toast.LENGTH_LONG);
+        View view = toast.getView();
+        int bg_color = Color.RED;
+        view.getBackground().setColorFilter(bg_color, PorterDuff.Mode.SRC_IN);
+        TextView text = view.findViewById(android.R.id.message);
+        text.setTextColor(Color.WHITE);
+        toast.show();
+    }
+
+    public static void InfoToast(Context context, String message){
+
+        Toast toast = Toast.makeText(context, message, Toast.LENGTH_LONG);
+        View view = toast.getView();
+        int bg_color = Color.YELLOW;
+        view.getBackground().setColorFilter(bg_color, PorterDuff.Mode.SRC_IN);
+        TextView text = view.findViewById(android.R.id.message);
+        text.setTextColor(Color.WHITE);
         toast.show();
     }
 }
