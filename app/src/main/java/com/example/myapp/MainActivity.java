d691b96fca776bc722a0b3f059917172171349cd
emranevan <77226225+emranevan@users.noreply.github.com>
2021-01-17 11:57:44 +0600

    2.0

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -6,11 +6,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.toastlibrary.Toaster;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toaster.toaster(MainActivity.this,"Hey");
+            }
+        });
     }
 }
--- toastlibrary/src/main/java/com/example/toastlibrary/Toaster.java
+++ toastlibrary/src/main/java/com/example/toastlibrary/Toaster.java
@@ -1,10 +1,17 @@
 package com.example.toastlibrary;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Toaster {
     public static void toaster(Context context, String message){
-        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+
+        Toast toast = Toast.makeText(context, message, Toast.LENGTH_LONG);
+        View view = toast.getView();
+        view.setBackgroundResource(R.drawable.toast_bg);
+        toast.show();
     }
 }
